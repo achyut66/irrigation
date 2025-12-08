86c0a1eecf743df0a7dd3bf991e3e912a8d6ea20
Achyut Neupane <green.band66@gmail.com>
2025-12-08 15:25:11 +0545

    Update AdminDashboardLayout for user authentication
    Fetch user data and redirect if not admin.

--- frontend/app/(admin)/admin-dashboard/layout.tsx
+++ frontend/app/(admin)/admin-dashboard/layout.tsx
@@ -1,9 +1,25 @@
-export default function AdminDashboardLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return <>{children}</>;
+import { redirect } from "next/navigation";
+
+export default async function AdminLayout({ children }) {
+  const API = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+  const res = await fetch(`${API}/api/user`, {
+    cache: "no-store",
+    credentials: "include",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    redirect("/admin-access");
   }
-  
-  
+
+  const user = await res.json();
+
+  if (!user?.isAdmin) {
+    redirect("/admin-access");
+  }
+
+  return <>{children}</>;
+}
